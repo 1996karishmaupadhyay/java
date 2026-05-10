24ab68243e919334b883010e582ba271a4f2250b
karishma upadhyay <1996karishmaupadhyay@gmail.com>
2026-05-10 23:53:09 +0530

    ArrayList

---
+++ ArrayLists.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class ArrayLists {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        System.out.println(list.get(2));
+        System.out.println(list.get(3));
+        System.out.println(list.get(4));
+    }
+}
