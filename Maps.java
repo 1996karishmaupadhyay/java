8748d07fec052e49d2334e53b946fbcea1ef1f7b
Karishma Upadhyay <1996karishmaupadhyay@gmail.com>
2026-05-14 15:18:08 +0530

    map started

---
+++ Maps.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Maps {
+   public static void main(String[] args) {
+     Map <Integer,String> mapp= new HashMap<>();
+      HashMap <String,String> mapp2= new HashMap<>();
+
+mapp.put(1,"Karishma");
+mapp.put(2, "naina");
+
+mapp.put(2, "naina");
+System.out.println(mapp);
+
+System.out.println(mapp.keySet());
+System.out.println(mapp.values());
+System.out.println(mapp.entrySet());
+   } 
+}
