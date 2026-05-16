8d3174ca1426e3e1eb70cd394f55c1fbb03465da
karishma upadhyay <1996karishmaupadhyay@gmail.com>
2026-05-16 17:31:33 +0530

    bits completed

--- Bits.java
+++ Bits.java
@@ -2,11 +2,28 @@ public class Bits {
     public static void main(String[] args) {
         int a=5;
         int b=6;
-        System.out.println(a&b); 
-        System.out.println(a|b);
-        System.out.println(a^b);
-        System.out.println(~a);
-          System.out.println(~b);
+        // System.out.println(a&b); 
+        // System.out.println(a|b);
+        // System.out.println(a^b);
+        // System.out.println(~a);
+        //   System.out.println(~b);
+//         int  n=5;
+//           for(int i=1;i<=32;i++){
+// // n=n<<1;
+// System.out.println(n);
+//           }
+//        int  n=100;
+//           for(int i=1;i<=10;i++){
+// n=n>>1;
+// System.out.println(n);
+//           }
 
+int n=2;
+if ((n & 1) == 1) {
+    System.out.println("odd");
+}
+else{
+    System.out.println("even");
+}
     }
 }
