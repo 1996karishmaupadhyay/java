6f476d64277285031e6bc50be363de24e863f26f
Karishma Upadhyay <1996karishmaupadhyay@gmail.com>
2026-06-05 17:55:35 +0530

    day8

---
+++ Day8.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Day8 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("type your input");
+     // String str=sc.next();
+       // System.out.println(str);
+        //System.out.println(str.length());
+        String s1=new String("Nobita");
+        String s2=new String("hbdkjhs");
+        System.out.println(s1+s2);
+        String s3=new String(" Try to work \"properly\"" );
+        System.out.println(s3 + "jnkbghki  "); 
+        int sizeofs3=s3.length();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+        System.out.println(sizeofs3);
+    }
+}
+          
