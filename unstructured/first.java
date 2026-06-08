ae73d3792681d1353dd1bca85f904e646fa28a96
karishma upadhyay <1996karishmaupadhyay@gmail.com>
2026-06-08 21:52:22 +0530

    ..

--- unstructured/first.java
+++ unstructured/first.java
@@ -1,5 +1,8 @@
 public class first {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        int a=7;
+        int b=9;
+        System.out.println(a+b);
     }
 }
